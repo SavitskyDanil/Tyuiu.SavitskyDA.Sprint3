a60fa2b91aed325267133906d87bfe441b76f4e9
Tyuiu.SavitskyDA@outlook.com <Tyuiu.SavitskyDA@outlook.com>
2023-10-29 17:35:18 +0500

    Task5

---
+++ Tyuiu.SavitskyDA.Sprint3.Task5.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SavitskyDA.Sprint3.Task5.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SavitskyDA.Sprint3.Task5.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("345a0362-17f1-4e66-b8c8-3636a49650a6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SavitskyDA.Sprint3.Task5.V3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SavitskyDA.Sprint3.Task5.V3.Lib;
+
+namespace Tyuiu.SavitskyDA.Sprint3.Task5.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Савицкий Д. А. | АСОиУб-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Савицкий Данил Александрович | АСОиУб-23-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Решить уравнение                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 5;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 11;
+
+            Console.WriteLine(" Переменная X = " + x);
+            Console.WriteLine(" Старт шага первой суммы ряда = " + startValue1);
+            Console.WriteLine(" Конец шага первой суммы ряда = " + stopValue1);
+            Console.WriteLine(" Старт шага второй суммы ряда = " + startValue2);
+            Console.WriteLine(" Конец шага второй суммы ряда = " + stopValue2);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(" Сумма ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+
+            Console.ReadLine();
+        }
+    }
+}
